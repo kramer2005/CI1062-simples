b739a91419e0a274efa3e919414964f520721ee3
Wagner J. Kramer Vieira <wjkv18@inf.ufpr.br>
2021-03-01 16:15:07 -0300

    Programa completo comentado

--- Programa.java
+++ Programa.java
@@ -4,23 +4,33 @@
 
 class Programa {
   public static void main(String[] args) {
+    // Cria pasta root
     Pasta root = new Pasta("root");
+
+    //Cria pastas texto, imagens e pdfs
     Pasta textos = new Pasta("texto");
     Pasta imagens = new Pasta("imagens");
     Pasta pdfs = new Pasta("pdfs");
+
+    // Adiciona 3 arquivos de cada tipo às suas respectivas pastas
     for (int i = 0; i < 3; i++) {
       textos.addFilho(new Arquivo(String.format("texto-%d.txt", i), "texto"));
       imagens.addFilho(new Arquivo(String.format("imagem-%d.jpg", i), "imagem"));
       pdfs.addFilho(new Arquivo(String.format("pdf-%d.pdf", i), "pdf"));
     }
+
+    // Adiciona as pastas à pasta root
     root.addFilho(textos);
     root.addFilho(imagens);
     root.addFilho(pdfs);
-    System.out.println("====================================================================\n");
+
+    // Imprime todas as pastas e arquivos com tipo
+    System.out.println("\n====================================================================\n");
     System.out.println("Imprimindo todas as pastas e arquivos (com tipo junto para facilitar\n");
     System.out.println("====================================================================\n");
     root.printFilhos();
 
+    // Imprime todos os arquivos de texto
     System.out.println("\n=====================================\n");
     System.out.println("Imprimindo todos os arquivos de texto\n");
     System.out.println("=====================================\n");
